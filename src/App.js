4c7858c90f780a6caf5e588a8c50774dcfefb198
amanchimania <amanchimania@gmail.com>
2021-05-10 22:40:07 +0530

    delete working now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([] || JSON.parse((localStorage.getItem("todoStore"))))
+  const [todo, setTodo] = useState({
+    title: "",
+    description: "",
+    completed: false
+  })
+  console.log(data);
+  const handleInput = (e) => {
+    // console.log(e);
+    setTodo({
+      ...todo,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleData = (e) => {
+    e.preventDefault();
+    let copyTodo = [...data]
+    copyTodo.push(todo);
+    setData(copyTodo)
+    localStorage.setItem("todoStore", JSON.stringify(data))
+    console.log(data);
+  }
+  const deleteTodo = (title) => {
+    console.log(title);
+    let deleteTodo = data.filter(val => val.title !== title)
+    console.log(deleteTodo);
+    setData(deleteTodo)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="Enter Title" name="title" onChange={handleInput} />
+        <input type="text" placeholder="Todo Description" name="description" onChange={handleInput} />
+        <input type="submit" onClick={handleData} />
+      </form>
+      {data.map((val, idx) => (
+        <div key={val.title}>
+          {val.title}
+          {val.description}
+          <button onClick={() => deleteTodo(val.title)}>Delete</button>
+        </div>
+      ))}
     </div>
+
   );
 }
 
